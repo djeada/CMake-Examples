77922512f5cb05ac9378abf9326852f3c725b73b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-03-03 00:01:02 +0100

    added example executable and a library

---
+++ src/ExecutableAndLibraries/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "complex.h"
+#include "vector.h"
+
+int main(){
+
+	Complex c1;
+	Complex c2(9, 12);
+	
+	std::cout << c1;
+	std::cout << c2;
+	
+	std::cout << c1 * c2;
+	
+	Complex c3(-3,-3);
+	
+	std::cout << c3 + c2;
+	std::cout << c3 / c2;
+	
+	c2.conjugate();
+	
+	std::cout << c2;
+	
+	std::cout << c3.magnitude() << std::endl;
+
+	Vector vectorA(5, 5, 5);
+	Vector vectorB(-3, -3, -3);
+
+	auto sum = vectorA + vectorB;
+	auto diff = vectorA - vectorB;
+	auto crossProd = vectorA * vectorB;
+
+	std::cout << "For vectors: A \n" << vectorA << "and B \n" << vectorB
+		<< "sum: \n" << sum << "difference: \n" << diff
+		<< "cross product: \n" << crossProd << std::endl;
+	
+	return 0;
+}
