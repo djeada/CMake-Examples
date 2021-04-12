3bb9cc558ba45a14c723751a2661eae4c040e8ce
djeada <addjellouli1@gmail.com>
2021-04-12 23:57:38 +0200

    formatting

--- src/ExecutableAndLibraries/complex.h
+++ src/ExecutableAndLibraries/complex.h
@@ -6,25 +6,24 @@
 using namespace std;
 
 class Complex {
-	private:
-		double Re;
-		double Im;
-	public:
-		Complex();
-		Complex(double a, double b);
-		
-		double magnitude();
-		void conjugate();
-		
-		friend Complex operator + (const Complex &c1, const Complex &c2);
-		friend Complex operator - (const Complex &c1, const Complex &c2);
-		friend Complex operator * (const Complex &c1, const Complex &c2);
-		friend Complex operator / (const Complex &c1, const Complex &c2);
-		friend bool operator == (const Complex &c1, const Complex &c2);
-		
-		friend ostream& operator << (ostream& out, const Complex &c);
+private:
+    double Re;
+    double Im;
+
+public:
+    Complex();
+    Complex(double a, double b);
+
+    double magnitude();
+    void conjugate();
+
+    friend Complex operator+(const Complex& c1, const Complex& c2);
+    friend Complex operator-(const Complex& c1, const Complex& c2);
+    friend Complex operator*(const Complex& c1, const Complex& c2);
+    friend Complex operator/(const Complex& c1, const Complex& c2);
+    friend bool operator==(const Complex& c1, const Complex& c2);
+
+    friend ostream& operator<<(ostream& out, const Complex& c);
 };
 
-
 #endif
-
