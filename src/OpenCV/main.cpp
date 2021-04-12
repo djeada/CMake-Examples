bd6b09a4e468a06ac33e6c9043dbce9965ae6e33
djeada <addjellouli1@gmail.com>
2021-04-12 23:57:17 +0200

    added opencv example

---
+++ src/OpenCV/main.cpp
@@ -0,0 +1,12 @@
+#include "opencv2/opencv.hpp"
+
+int main()
+{
+
+    cv::Mat img = cv::imread("example.png", cv::IMREAD_UNCHANGED);
+    cv::rectangle(img, cv::Point(0, 0), cv::Point(400, 200), cv::Scalar(0, 0, 0, 255), -1);
+    cv::putText(img, "Transistor", cv::Point(50, 100), cv::FONT_ITALIC, 2.0, cv::Scalar(255, 255, 255, 255), 2);
+    cv::imwrite("example_texts.png", img);
+
+    return 0;
+}
